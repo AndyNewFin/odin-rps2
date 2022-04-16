e9ffc8a64e1b371b4bd3e6749f60aeea4910a0b8
AndyNewFin <andy_cnguyen@outlook.com>
2022-04-16 14:56:34 -0700

    index.html

--- README.md
+++ README.md
@@ -1,9 +1,28 @@
-# odin-rps24/9/2022
+# odin-rps2 4/9/2022
 File backup created to make sure original files are safe before editing/branch
 
 I am starting the Revisiting Rock-Paper-Scissor project where I need to initially create a branch on github
 
 I decided to create a new repo: odin-rps2, copy files over, then create a new branch to the new repo
+
+
+4/16/2022
+Note: The script.js file does NOT work - I tried to
+     1) Code on my own with help from TOP instructions; Google; TOP Discord; Stack Overflow, but could not get anything to work with my current code
+     2) Reviewed some TOP solutions and get some inspiration, but again, could not get anything to work with my current code
+     3) After nearly a week, I decided it was best to start this project from using with help from YouTube's "Jaybees Tech Talk"
+	3a) File script2.js is basically a me watching Jaybees Tech Talk video "The Odin Project VLOG | Day 51 CODING For BEGINNERS | JavaScript Project Rock Paper Scissors REVISED" and coding along with him
+		url: https://www.youtube.com/watch?v=k57un0aHjgs
+		
+I am hoping I do have to do this too much in future projects since I believe it is best to learn via my own code and solutions.
+
+Positive note: I am happy that I understood and familiar with most of what Jaybees Tech Talk had to teach! I reviewed each line of code to understand what each line did; how each line connects; and how all the lines come together to solve this project
+
+
+Need to work on:
+1) Slowing down to carefully make sure no typos/mispelling and that punctuation marks are correct
+2) Understating that some keywords are CASE-sensitive: in the button.addEventListener, I had "Click" instead of "click" which caused it not to work
+3) Dom methods: This project heavily emphasized DOM methods to add/update the html... I originally had a style.css file 
 
 
 
--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+// Note: This JS file does NOT work - I tried to
+    // 1) Code on my own with help from Google; TOP Discord; Stack Overflow, but could not get anything to work with my current code
+    // 2) Reviewed some TOP solutions and get some inspiration, but again, could not get anything to work with my current code
+    // 3) After nearly a week, I decided it was best to start this project from using with help from YouTube's Jaybees Tech Talk
+
+
 // JS F4: odin-rps2
 
 // function computerPlay = randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’
@@ -48,6 +54,7 @@ function rps(selection) {
     }
 }
 
+
 // i is the number of matches; the first match is zero per JS counting rules
 let i = 0;
 
@@ -55,6 +62,7 @@ let i = 0;
 let playerScore = 0;
 let computerScore = 0;
 
+/* rps2: 2a - remove the logic that plays 5 rounds
 // this loops runs 5 matches and displays the results in the console.log
 while (i < 5){
     let fight = prompt("TYPE: Rock, Paper, or Scissors!");
@@ -63,6 +71,90 @@ while (i < 5){
     console.log(`Player score: ${playerScore} & Computer score: ${computerScore}`);
     i++
 }
+rps2: 2a */
+
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        playerSelection = button.className;
+        const computerSelection = computerPlay();
+        battleWinText.textContent = (playeRound(playerSelection, computerSelection));
+        playerWinText.textContent = "Player Win totals: " + playerWin;
+        computerWinText.textContent = "Computer Win totals: " + computerWin;
+        endGame();
+    })
+})
+
+    // create div DOM for all results
+    const container = document.querySelector("#container");
+    const resultsDiv = document.createElement("div");
+    resultsDiv.style.marginTop = "20px",
+    container.appendChild(resultsDiv);
+
+    // create player win tracking DOM
+    const playerWinText = document.createElement("p");
+    playerWinText.style.color = "blue";
+    playerWinText.textContent = "Player Win totals: " + playerWin;
+    resultsDiv.appendChild(playerWinText);
+
+    // create computer win tracking DOM
+    const computerWinText = document.createElement("p");
+    computerWinText.style.color = "blue";
+    computerWinText.textContent = "Computer Win totals: " + computerWin;
+    resultsDiv.appendChild(computerWinText);
+
+    // create battle win text DOM
+    const battleWinText = document.createElement("p");
+    battleWinText.style.color = "black";
+    resultsDiv.appendChild(battleWinText);
+
+    // create game win text DOM
+    const gameWinText = document.createElement("p");
+    gameWinText.textContent = gameWinner;
+    gameWinText.style.color = "orange";
+    resultsDiv.appendChild(gameWinText);
+
+    // determine who won to five points first
+    function endGame() {
+        if (playerWin == 5) {
+            gameWinner = "You Win!";
+            gameWinText.textContent = gameWinner;
+
+            // disable game buttons
+            document.getElementById("1").disabled = true;
+            document.getElementById("2").disabled = true;
+            document.getElementById("3").disabled = true;
+
+            // create new DOM button to replay
+            const playAgainButton = document.createElement("button");
+            playAgainButton.textContent = "Play Again!";
+            resultsDiv.appendChild(playAgainButton);
+
+            // if clicked, reload page
+            playAgainButton.addEventListener('click', () => {
+                location.reload();
+            })
+        } else if (computer == 5) {
+            gameWinner = "Computer Wins!";
+            gameWinText.textContent = gameWinner;
+
+            // disable game buttons
+            document.getElementById("1").disabled = true;
+            document.getElementById("2").disabled = true;
+            document.getElementById("3").disabled = true;
+
+            // create new DOM button to replay
+            const playAgainButton = document.createElement("button");
+            playAgainButton.textContent = "Play Again!";
+            resultsDiv.appendChild(playAgainButton);
+
+            // if clicked, reload page
+            playAgainButton.addEventListener('click', () => {
+                location.reload();
+            })    
+        }
+    }
+
 
 // after 5 matches, the "if" compares playerScore to computerScore to see if the playerScore is greater/les than computerScore
 if (playerScore > computerScore){
